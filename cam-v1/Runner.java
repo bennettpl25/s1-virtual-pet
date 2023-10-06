ba23d78a51015ec59d7ada9724849b7e89b43be8
bennettpl25 <bennettpl25@ahs.local>
2023-10-06 15:05:14 -0700

    23454terhsyse

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,6 +1,32 @@
+import javax.swing.*;
 public class Runner{
-    public static void main(String[] args){
+    public Runner(){
         VirtualPet p = new VirtualPet();
         p.exercise();
+        takeBreak(1000);
+        String ans = getResponse("How was your day?");
+        System.out.println(ans);
+        p.exercise();
+        p.exercise();
+        p.exercise();
+        takeBreak(1000);
+        p.sleep();
+
+    }
+    public String getResponse(String q){
+        String s = (String)JOptionPane.showInputDialog(
+            new JFrame(),
+            q,
+            "Get a Response",
+            JOptionPane.PLAIN_MESSAGE);
+            return s;
+    }
+    public void takeBreak(int milliseconds){
+         try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){}
+    }
+    public static void main(String[] args){
+        new Runner();
     }
 }
