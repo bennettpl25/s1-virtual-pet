e41918081c4a260583e05f267fe61cf50d3806cf
bennettpl25 <bennettpl25@ahs.local>
2023-10-13 15:05:11 -0700

    gbfdsbngfssfv

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -9,13 +9,29 @@ public class Runner{
         else{
             takeBreak(1000);
             p.houseFront();
-            takeBreak(2000);
-            if (getResponse2() == 0){p.basementEnding();takeBreak(2000);p.astonishment();takeBreak(2000); p.death();}
-            else if (getResponse2() == 1){p.livingroom();
-                
+            takeBreak(1000);
+            int gr2 = getResponse2();
+            if (gr2 == 0){p.basementEnding();takeBreak(2000);p.astonishment();takeBreak(2000); p.death();}
+            if (gr2 == 1){p.livingroom();
+                takeBreak(2000);
+                int gr3 = getResponse3(); 
+                if (gr3 == 0){p.closetHole(); takeBreak(2000); int gr4 = getResponse4(); 
+                    if(gr4 == 0){p.closetJump(); takeBreak(2000); p.death();}
+                    if(gr4 == 1){p.closetDontJump(); takeBreak(2000); p.astonishment(); takeBreak(1000); p.closetMonster(); takeBreak(1000); p.death();}
+            }
+            if (gr3 == 1){p.nothing(); takeBreak(2000); p.astonishment(); takeBreak(1000); p.livingMonster(); takeBreak(1000); p.death();
+            }
+            }
+            if (gr2 == 2){p.bedroom(); takeBreak(2000); int gr5 = getResponse5();
+                if (gr5 == 0){
+                    p.tired(); takeBreak(1000); p.money();
+                    int gr6 = getResponse6();
+                    if (gr6 == 0){p.moneyUp();}
+                }
+            p.leave();
+
             }
         }
-
         }
 
     public int getResponse1(){
@@ -41,6 +57,60 @@ public class Runner{
     null,
     options,
     options[2]);
+
+    return n;
+    }
+
+    public int getResponse3(){
+        Object[] options = {"Search Closet", "Don't Search Closet"};
+    int n = JOptionPane.showOptionDialog(new JFrame(), 
+    "Search Closet?",
+    "Livingroom",
+    JOptionPane.YES_NO_CANCEL_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,
+    options,
+    options[1]);
+
+    return n;
+    }
+    public int getResponse4(){
+        Object[] options = {"Jump in Hole", "Don't Jump in Hole"};
+    int n = JOptionPane.showOptionDialog(new JFrame(), 
+    "Jump in Hole?",
+    "Closet",
+    JOptionPane.YES_NO_CANCEL_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,
+    options,
+    options[1]);
+
+    return n;
+    }
+    public int getResponse5(){
+        Object[] options = {"Sleep in Bed", "Don't Sleep in Bed"};
+    int n = JOptionPane.showOptionDialog(new JFrame(), 
+    "Sleep?",
+    "Bedroom",
+    JOptionPane.YES_NO_CANCEL_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,
+    options,
+    options[1]);
+
+    return n;
+    }
+
+    public int getResponse6(){
+        Object[] options = {"Take Money", "Don't Take Money"};
+    int n = JOptionPane.showOptionDialog(new JFrame(), 
+    "Money?",
+    "Bedroom",
+    JOptionPane.YES_NO_CANCEL_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,
+    options,
+    options[1]);
 
     return n;
     }
--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -6,9 +6,7 @@
 public class VirtualPet {
     
     VirtualPetFace face;
-    private int hunger = 0;   // how hungry the pet is.
-    private int tiredness = 0;
-    // constructor
+    private int money;
     public VirtualPet() {
         face = new VirtualPetFace();
         face.setImage("normal");
@@ -36,7 +34,49 @@ public class VirtualPet {
         }
         public void livingroom(){
             face.setMessage("Yonkers! What a very creepy scary living room! Oh look, a very suspucious closet!");
+            face.setImage("livingroom");
+        }
+        public void closetHole(){
+            face.setMessage("wow, a cool hole in the floor? I wanna jump in!");
+            face.setImage("closet");
+        }
+        public void closetJump(){
+            face.setMessage("AAAAAAAAAAAAAAAHHH");
             face.setImage("shocked");
         }
+        public void closetDontJump(){
+            face.setMessage("I probably shouldn't jump. I'll just turn around...");
+            face.setImage("happy");
+        }
+        public void closetMonster(){
+            face.setImage("cl_monster");
+        }
+        public void livingMonster(){
+            face.setImage("lrmonster");
+        }
+        public void nothing(){
+            face.setMessage("Wow, there's absolutely nothing in this room");
+            face.setImage("sad");
+        }
+        public void bedroom(){
+            face.setMessage("Wowzers, pretty normal bedroom. I'm really tired, I think I want to sleep");
+            face.setImage("bedroom");
+        }
+        public void tired(){
+            face.setImage("tired");
+            face.setMessage("YAWN");
+        }
+        public void money(){
+            face.setImage("ecstatic");
+            face.setMessage("I feel something... OH! It's a ton of CASH!");
+        }
+        public void moneyUp(){
+            money = 9999;
+        }
+        public void leave(){
+            face.setMessage("Well, I guess there's nothing else to see. Time to leave.");
+            face.setImage("ecstatic");
+        }
+
 }
  // end Virtual Pet
